b2b7a458ef876a382a7fe287d77b2f30d35df0b1
Nadia Jazi <jazinadia7@gmail.com>
2024-02-16 21:24:15 +0100

    Ajoutez des fichiers projet.

---
+++ WebApplication1/Controllers/ProducersController.cs
@@ -0,0 +1,172 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using WebApplication1.Models.Cinema;
+
+namespace WebApplication1.Controllers
+
+{
+
+    public class ProducersController : Controller
+    {
+        CinemaDbContext _context;
+
+        public ProducersController(CinemaDbContext context)
+        {
+            _context = context;
+
+        }
+
+
+
+
+        // GET: ProducersController
+        public ActionResult Index()
+        {
+            var producers = _context.Producers.ToList();
+            return View(producers);
+        }
+
+
+
+
+
+
+
+
+        // GET: ProducersController/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+
+
+
+
+
+        // POST: ProducersController/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(Producer producer)
+        {
+            try
+            {
+
+                _context.Producers.Add(producer);
+                _context.SaveChanges();
+
+                return RedirectToAction(nameof(Index)); // Redirigez vers l'index après la création réussie
+
+            }
+            catch (Exception ex)
+            {
+
+                ModelState.AddModelError("", "Une erreur s'est produite lors de la création du producteur.");
+            }
+
+            return View(producer);
+        }
+
+
+
+
+
+
+        // GET: ProducersController/Edit/5
+        public ActionResult Edit(int id)
+        {
+            var producer = _context.Producers.Find(id);
+            if (producer == null)
+            {
+                return NotFound();
+            }
+
+            return View(producer);
+        }
+
+        // POST: ProducersController/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(int id, Producer producer)
+        {
+            if (id != producer.Id)
+            {
+                return BadRequest();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Producers.Update(producer);
+                    _context.SaveChanges();
+                    return RedirectToAction(nameof(Index));
+                }
+                catch
+                {
+                    return View(producer);
+                }
+            }
+
+            // Retourne la vue avec le modèle en cas de ModelState invalide
+            return View(producer);
+        }
+
+
+        // GET: ProducersController/Delete/5
+        public ActionResult Delete(int id)
+        {
+            var producer = _context.Producers.Find(id);
+            if (producer == null)
+            {
+                return NotFound();
+            }
+
+            return View(producer);
+        }
+        // POST: ProducersController/Delete/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeletePost(int id)
+        {
+            try
+            {
+
+                var producer = _context.Producers.Find(id);
+
+                if (producer == null)
+                {
+                    return NotFound();
+                }
+
+
+                _context.Producers.Remove(producer);
+                _context.SaveChanges();
+
+                return RedirectToAction(nameof(Index));
+            }
+            catch (Exception ex)
+            {
+
+                return RedirectToAction(nameof(Index));
+            }
+
+
+        }
+
+        // GET: ProducersController/Details/5
+        public ActionResult Details(int id)
+        {
+            var producer = _context.Producers.Find(id);
+            if (producer == null)
+            {
+                return NotFound();
+            }
+            return View(producer);
+        }
+
+
+    }
+
+
+}
---
+++ WebApplication1/Models/Cinema/CinemaDbContext.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+
+namespace WebApplication1.Models.Cinema;
+
+public partial class CinemaDbContext : DbContext
+{
+    public CinemaDbContext()
+    {
+    }
+
+    public CinemaDbContext(DbContextOptions<CinemaDbContext> options)
+        : base(options)
+    {
+    }
+
+    public virtual DbSet<Movie> Movies { get; set; }
+
+    public virtual DbSet<Producer> Producers { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        => optionsBuilder.UseSqlServer("Name=ConnectionStrings:CinemaCS");
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Movie>(entity =>
+        {
+            entity.HasKey(e => e.Id).HasName("PK__Movie__3214EC07BF5C1C46");
+
+            entity.ToTable("Movie");
+
+            entity.Property(e => e.Genre)
+                .HasMaxLength(30)
+                .IsFixedLength();
+            entity.Property(e => e.Title)
+                .HasMaxLength(30)
+                .IsFixedLength();
+
+            entity.HasOne(d => d.Producer).WithMany(p => p.Movies)
+                .HasForeignKey(d => d.ProducerId)
+                .OnDelete(DeleteBehavior.ClientSetNull)
+                .HasConstraintName("FK__Movie__ProducerI__38996AB5");
+        });
+
+        modelBuilder.Entity<Producer>(entity =>
+        {
+            entity.HasKey(e => e.Id).HasName("PK__Producer__3214EC0756201DCA");
+
+            entity.ToTable("Producer");
+
+            entity.Property(e => e.Email).HasMaxLength(30);
+            entity.Property(e => e.Name).HasMaxLength(30);
+            entity.Property(e => e.Nationality).HasMaxLength(30);
+        });
+
+        OnModelCreatingPartial(modelBuilder);
+    }
+
+    partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+}
